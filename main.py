5914081e75b39a9fd119d10e312eabe489056eda
Damien Ouzillou <damien.ouzillou@student-cs.fr>
2025-10-23 12:43:45 +0200

    Step2: parsing ABI json file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import json
 from src.ingest_contract import get_contract_data
+from src.analyze_contract import analyze_contract
 
 
 def main():
     # USDC contract address
     address = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
     contract = get_contract_data(address)
+    summary = analyze_contract(address)
+    print(json.dumps(summary, indent=2))
     print("Code finished")
 
 
---
+++ src/analyze_contract.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+DATA_DIR = "data"
+
+
+def load_abi(address: str):
+    path = os.path.join(DATA_DIR, f"{address}_abi.json")
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"ABI file not found for address {address}")
+
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def analyze_contract(address: str):
+    abi = load_abi(address)
+
+    functions = [item for item in abi if item["type"] == "function"]
+    events = [item for item in abi if item["type"] == "event"]
+    constructors = [item for item in abi if item["type"] == "constructor"]
+    fallbacks = [item for item in abi if item["type"] == "fallback"]
+
+    public_functions = [func["name"] for func in functions]
+    view_functions = [func["name"] for func in functions if func.get("stateMutability") == "view"]
+    payable_functions = [func["name"] for func in functions if func.get("stateMutability") == "payable"]
+    restricted_functions = [func["name"] for func in functions if any(keyword in func["name"].lower() for keyword in ["admin", "owner"])]
+    emitted_events = [ev["name"] for ev in events]
+
+    return {
+        "total_functions": len(functions),
+        "total_events": len(events),
+        "total_constructors": len(constructors),
+        "total_fallbacks": len(fallbacks),
+        "public_functions": public_functions,
+        "view_functions": view_functions,
+        "payable_functions": payable_functions,
+        "owner_related_functions": restricted_functions,
+        "emitted_events": emitted_events
+    }
